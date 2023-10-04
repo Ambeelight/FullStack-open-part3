7b2304a87e095283dcc87dd75a2eb1e2a86cfdbb
Makar Istratov <imakar98@gmail.com>
2023-10-05 00:52:46 +0300

    Completed exercises 3.2-3.6

--- Phonebook/index.js
+++ Phonebook/index.js
@@ -6,7 +6,9 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-let data = [
+app.use(express.json());
+
+let persons = [
     {
         id: 1,
         name: "Arto Hellas",
@@ -33,6 +35,57 @@ app.get("/", (req, res) => {
     res.send("Hello world");
 });
 
+app.get("/info", (req, res) => {
+    const currentTime = new Date().toString();
+    res.send(`
+    <p>Phonebook has info for ${data.length} people</p>
+    <p>${currentTime}</p>`);
+});
+
 app.get("/api/persons", (req, res) => {
-    res.json(data);
+    res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find((person) => person.id === id);
+
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter((person) => person.id !== id);
+
+    res.status(204).end();
+});
+
+const generateRandomId = (max) => Math.floor(Math.random() * (max + 1));
+
+app.post("/api/persons", (req, res) => {
+    const body = req.body;
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: "Name or number is missing",
+        });
+    } else if (persons.some((person) => person.name === body.name)) {
+        return res.status(400).json({
+            error: "Name must be unique",
+        });
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateRandomId(999),
+    };
+
+    persons = persons.concat(person);
+
+    res.json(persons);
 });
